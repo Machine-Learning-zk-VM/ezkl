7d94f494d6ab6c4a1fee86a592466e0b2fee4d87
Georg Wiese <georgwiese@gmail.com>
2023-11-12 06:46:23 +0000

    Refactor

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub mod eth;
 pub mod execute;
 /// Utilities for converting from Halo2 Field types to integers (and vice-versa).
 pub mod fieldutils;
+mod generate_vm;
 /// Methods for loading onnx format models and automatically laying them out in
 /// a Halo2 circuit.
 #[cfg(feature = "onnx")]
